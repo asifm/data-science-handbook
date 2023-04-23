dc97a5fbe2bb87ec85ebc3586af5bed6e54a4108
Asif Mehedi <asif.mehedi@live.com>
2023-04-23 18:54:43 -0400

    Add callout

--- .app/.eleventy.js
+++ .app/.eleventy.js
@@ -1,17 +1,23 @@
 const loader = require("./_config/utils/loader.util");
 
 module.exports = (eleventyConfig) => {
-  // Load eleventy configurations from './_config' folder
-  loader([__dirname, "_config"], eleventyConfig);
+    // Load eleventy configurations from './_config' folder
+    loader([__dirname, '_config'], eleventyConfig);
 
-  return {
-    pathPrefix: process.env.ELEVENTY_NOTES_PATH_PREFIX || undefined,
-    dir: {
-      input: "./../",
-      output: "dist",
-      data: ".app/_data",
-      includes: ".app/_includes",
-    },
-    markdownTemplateEngine: false,
-  };
+    eleventyConfig.addPairedShortcode('callout', function (content, calloutClass) {
+        return `<div class="callout ${calloutClass}">
+                <p>${content}</p>
+                </div>`;
+    });
+
+    return {
+        pathPrefix: process.env.ELEVENTY_NOTES_PATH_PREFIX || undefined,
+        dir: {
+            input: './../',
+            output: 'dist',
+            data: '.app/_data',
+            includes: '.app/_includes',
+        },
+        // markdownTemplateEngine: false,
+    };
 };
